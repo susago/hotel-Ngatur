1cda2a9ae6c3b467f81bc558db007c3f0f408a96
susago <santyrod2004@gmail.com>
2024-08-22 20:43:52 -0500

    formulario del hotel

---
+++ formulario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style_registro.css">
+    <title>formulario</title>
+</head>
+<body>
+<div class="container">
+    <div class="overlay" id="overlay">
+        <div class="sign-in" id="sign-in">
+    <form action="" method="POST">
+        <h2>formulario de registro</h2>
+
+        <label>nombre completo</label><br>
+        <input type="text" name="nombre_completo" id="nombre_completo">
+        <br>
+
+        <label>documento</label><br>
+        <input type="number" name="documento" id="documento">
+        <br>
+        <label>telefono</label><br>
+        <input type="number" name="telefono" id="telefono">
+        <br>
+        <label>correo</label><br>
+        <input type="email" name="email" id="email">
+        <br>
+        <label>establece tu contraseña</label><br>
+        <input type="password" name="contraseña" id="contraseña">
+        <br>
+    </form>
+</div>
+</body>
+</html>
